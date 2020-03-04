c8b4dce5bd52ed8b46b1d192d569501f8c9a4b18
Bianwy <bianwy025@qq.com>
2020-03-04 23:12:19 +0800

    '实现入参的测试。'

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const ref = require('ref-napi');
 // 测试 vs 的 dll
 const bytePtr = ref.refType(ref.types.uchar);
 const dwPtr = ref.refType(ref.types.ulong);
+const voidPtr = ref.refType(ref.types.void);
 const libFetchPictureAndTemp = ffi.Library('fetchPictureAndTemp.dll', {
   Add: ['int', ['int', 'int']],
-  testInvokeCallback: ['void', ['int', 'int', bytePtr, dwPtr]]
+  testInvokeCallback: ['void', ['int', 'int', bytePtr, dwPtr]],
+  CICDI_CLIENT_FetchPicture_DetectTemp: [
+    'int',
+    ['longlong', 'int', 'ulong', 'bool', 'uint', voidPtr]
+  ]
 });
 const libFetchPictureAndTempResult = libFetchPictureAndTemp.Add(2, 3);
 console.log('libFetchPictureAndTempResult:', libFetchPictureAndTempResult);
@@ -19,3 +24,13 @@ libFetchPictureAndTemp.testInvokeCallback(9, 8, buffer4, outSizePtr);
 console.log('buffer4', buffer4);
 const aa = outSizePtr.deref();
 console.log('outSizePtr->', aa);
+
+let voidVar = ref.alloc(voidPtr);
+libFetchPictureAndTemp.CICDI_CLIENT_FetchPicture_DetectTemp(
+  0,
+  1,
+  1,
+  true,
+  110,
+  voidVar
+);
