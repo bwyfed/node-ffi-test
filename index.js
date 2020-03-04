b0a0dde14828351de83ed74bacfdaf34106e5f3e
Bianwy <bianwy025@qq.com>
2020-03-04 19:17:54 +0800

    '实现测试C语言中自定义类型。'

--- index.js
+++ index.js
@@ -106,8 +106,24 @@ console.log(
 );
 console.log(tv);
 
-// console.log(`addResult:${addResult}, subResult:${subResult}`);
+// 测试 vs 的 dll
+const bytePtr = ref.refType(ref.types.uchar);
+const dwPtr = ref.refType(ref.types.ulong);
+const libFetchPictureAndTemp = ffi.Library('fetchPictureAndTemp.dll', {
+  Add: ['int', ['int', 'int']],
+  testInvokeCallback: ['void', ['int', 'int', bytePtr, dwPtr]]
+});
+const libFetchPictureAndTempResult = libFetchPictureAndTemp.Add(2, 3);
+console.log('libFetchPictureAndTempResult:', libFetchPictureAndTempResult);
+console.log('js call testInvokeCallback');
 
+const allocSize = 10;
+const buffer4 = Buffer.alloc(allocSize);
+const outSizePtr = ref.alloc(ref.types.ulong);
+libFetchPictureAndTemp.testInvokeCallback(9, 8, buffer4, outSizePtr);
+console.log('buffer4', buffer4);
+const aa = outSizePtr.deref();
+console.log('outSizePtr->', aa);
 // const libm = ffi.Library('mydll.dll', {
 //   Add: ['int', ['int', 'int']],
 //   Sub: ['int', ['int', 'int']]
