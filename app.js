40edea6be0dabc2a161c6b4017e19df9b1ff9018
Bianwy <bianwy025@qq.com>
2020-03-05 00:05:41 +0800

    '实现返回数组参数的功能。'

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ const ref = require('ref-napi');
 const bytePtr = ref.refType(ref.types.uchar);
 const dwPtr = ref.refType(ref.types.ulong);
 const voidPtr = ref.refType(ref.types.void);
+const uintPtr = ref.refType(ref.types.uint);
 const libFetchPictureAndTemp = ffi.Library('fetchPictureAndTemp.dll', {
   Add: ['int', ['int', 'int']],
   testInvokeCallback: ['void', ['int', 'int', bytePtr, dwPtr]],
   CICDI_CLIENT_FetchPicture_DetectTemp: [
     'int',
-    ['longlong', 'int', 'ulong', 'bool', 'uint', voidPtr]
+    [
+      'longlong',
+      'int',
+      'ulong',
+      'bool',
+      'uint',
+      voidPtr,
+      voidPtr,
+      bytePtr,
+      uintPtr
+    ]
   ]
 });
 const libFetchPictureAndTempResult = libFetchPictureAndTemp.Add(2, 3);
@@ -26,11 +37,21 @@ const aa = outSizePtr.deref();
 console.log('outSizePtr->', aa);
 
 let voidVar = ref.alloc(voidPtr);
+let voidOutVar = ref.alloc(voidPtr);
+let byteOutVar = ref.alloc(bytePtr);
+const buffer5 = Buffer.alloc(12); // 分配空间
+let buffer5Size = ref.alloc(ref.types.uint); // 这里千万不要写成ref.alloc(uintPtr)
 libFetchPictureAndTemp.CICDI_CLIENT_FetchPicture_DetectTemp(
   0,
   1,
   1,
   true,
   110,
-  voidVar
+  voidVar,
+  voidOutVar,
+  buffer5,
+  buffer5Size
 );
+console.log(buffer5);
+const bb = buffer5Size.deref();
+console.log('buffer5Size.deref()', bb);
